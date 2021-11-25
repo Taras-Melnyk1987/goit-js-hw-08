fa3c30ac9932a188866e07b7c0ce65d2b00350c1
Taras-Melnyk1987 <dr.melnuk1987@gmail.com>
2021-11-25 12:37:31 +0200

    add 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryImages = createGalleryItems(galleryItems);
+
+function createGalleryItems(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" style='display: inline-block; height: 240px' href=${original}>
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a>`,
+    )
+    .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', galleryImages);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
